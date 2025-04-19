a00ff53539e1f97549e110ff808fdf4c220ea444
Kiyonory <ltm5607@gmail.com>
2025-04-19 13:40:20 +0300

    lab2.1

--- lab2.1/index.php
+++ lab2.1/index.php
@@ -1,30 +1,44 @@
-<?php
-function solveEquation($equation) {
-    $parts = explode(' ', $equation);
-    $left = $parts[0];
-    $operator = $parts[1];
-    $right = $parts[2];
-    $result = $parts[4];
+<?php 
+echo "Задание 1<br>";
 
-    if ($right === 'x') {
-        switch ($operator) {
-            case '+': $x = $result - $left; break;
-            case '-': $x = $left - $result; break;
-            case '*': $x = $result / $left; break;
-            case '/': $x = $left / $result; break;
-        }
-    } elseif ($left === 'x') {
-        switch ($operator) {
-            case '+': $x = $result - $right; break;
-            case '-': $x = $result + $right; break;
-            case '*': $x = $result / $right; break;
-            case '/': $x = $result * $right; break;
-        }
-    }
+$arr = ['a', 'b', 'c', 'b', 'a'];
+$result = array_count_values($arr);
 
-    return $x;
+foreach ($result as $key => $count) {
+    echo "$key встречается $count раз(а)<br>";
 }
 
-$equation = "22 * x = 220";
-echo "x = " . solveEquation($equation);
+echo "<br>Задание 2<br>";
+$arr2 =  ['a'=>1, 'b'=>2, 'c'=>3];
+$result2 = array_flip($arr2);
+$result2_1 = array_reverse($arr2);
+echo "Массив: ";
+print_r($arr2);
+echo "<br>";
+echo "array_flip: ";
+print_r($result2);
+echo "<br>";
+echo "array_reverse: ";
+print_r($result2_1);
+echo "<br>";
+
+echo "<br>Задание 3<br>";
+$arr3 = [1, 2, 3, 4, 5];
+$result3 = array_reverse($arr3);
+foreach ($result3 as $key => $number) {
+    echo "$number";
+}
+echo "<br>";
+
+echo "<br>Задание 4<br>";
+$arr4 = ['a', 'b', 'c', 'd', 'e'];
+$result4 = array_map('strtoupper', $arr4);
+print_r($result4);
+echo "<br>";
+
+echo "<br>Задание 5<br>";
+$arr5 = [1, 2, 3, 4, 5];
+array_splice($arr5, 3, 0, ['a', 'b', 'c']);
+$arr5_nice = implode(",", $arr5);
+echo $arr5_nice;
 ?>
